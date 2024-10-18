84875a837885a20e0b780e414083492a451db9b5
Satrience <50034413+VLALENCE@users.noreply.github.com>
2024-10-18 16:01:27 -0500

    Updater > git files changed

--- exec_aud.bat
+++ exec_aud.bat
@@ -1,7 +1,7 @@
 echo Test env.
 cd /d C:\Users\offic\Documents\Github\Project\alapha-universe-docs
 git add --all
-for /f %%i in ('git diff --shortstat') do set VARIABLE=%%i
+for /f %%i in ("git diff --shortstat") do set VARIABLE=%%i
 echo %VARIABLE%
 git commit -m "Updater > %VARIABLE% files changed"
 git push origin SiteAPI
