e5dbf294bcdcfa7445b96023b4ededa259d9cc2f
Satrience <50034413+VLALENCE@users.noreply.github.com>
2024-10-18 16:23:48 -0500

    Updater >  2 files changed, 5 insertions(+), 5 deletions(-)

--- exec_aud.bat
+++ exec_aud.bat
@@ -1,8 +1,8 @@
 echo Test env.
 cd /d C:\Users\offic\Documents\Github\Project\alapha-universe-docs
 git add --all
-for /f %%i in ("git diff --shortstat") do set VARIABLE=%%i
-echo %VARIABLE%
-git commit -m "Updater > %VARIABLE% files changed"
+git diff --shortstat @~ > difference.txt
+set /p files_changed=<difference.txt
+echo %files_changed%
+git commit -m "Updater > %files_changed%"
 git push origin SiteAPI
-pause
