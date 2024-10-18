61b1e92c146847d3342d3527e581faa0bd72e454
Satrience <50034413+VLALENCE@users.noreply.github.com>
2024-10-18 16:00:17 -0500

    Updater >  files changed

--- exec_aud.bat
+++ exec_aud.bat
@@ -1,5 +1,8 @@
 echo Test env.
 cd /d C:\Users\offic\Documents\Github\Project\alapha-universe-docs
 git add --all
-git commit -m "Vlale updateDocs.js Update"
+for /f %%i in ('git diff --shortstat') do set VARIABLE=%%i
+echo %VARIABLE%
+git commit -m "Updater > %VARIABLE% files changed"
 git push origin SiteAPI
+pause
